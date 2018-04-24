beaea5e799f765f942d5994c22d60b21310771a7
Giulliano Morroni <giulliano.morroni@yara.com>
2018-04-24 10:35:43 -0300

    teste

--- domain/decision_tree.py
+++ domain/decision_tree.py
@@ -4,7 +4,7 @@
 
 
 from sklearn.externals import joblib
-from machine_learning.laboratory_A_and_L import PotatoPlantingDate
+from machine_learning.laboratory_A_and_L import PotatoCropCode
 from machine_learning import DecisionTreeVersion
 from database.repository import DatabaseRepository
 from domain import elements as domain_element
@@ -22,18 +22,18 @@ class InFieldDecisionTree:
 
         recommendations = []
         for data in records:
-            id_field_analyses = data[0]
-            #id_field = data[1]
-            #id_laboratory = data[2]
             json_lab_analyses = data[3]
-            variety = data[4]
-            crop = data[5]
-            planting_date = data[6]
 
-            sample_type = json_lab_analyses["SAMPLETYPE"]
-            crop_name = PotatoPlantingDate.season_by_planting_date(planting_date, crop, variety)
+            #este codigo veio da A&L...futuramente vamos ter q trocar
+            crop_code = json_lab_analyses["CROPCODE"]
+            crop_info = PotatoCropCode.code[crop_code]
+            crop = crop_info["crop"]
+            variety = crop_info["variety"]
+            crop_name = '{0} ((1)) ({2})'.format(crop.upper(), variety.upper(), crop_code)
 
-            if crop_name is None:
+            sample_type = crop_info["sample_type"]
+
+            if crop is None:
                 return
 
             if sample_type.lower() == 'soil':
@@ -47,9 +47,9 @@ class InFieldDecisionTree:
             element_le = joblib.load('machine_learning/laboratory_A_and_L/models/PotatoDecisionTree_ElementLabelEncode.pkl')
             crop_le = joblib.load('machine_learning/laboratory_A_and_L/models/PotatoDecisionTree_CropNameLabelEncode.pkl')
 
-            CURRENT_VERSION = DecisionTreeVersion.CURRENT_VERSION['laboratory_A_and_L']
-            model_version = CURRENT_VERSION['potato']['version']
-            model_name = CURRENT_VERSION['potato']['name']
+            _version = DecisionTreeVersion.CURRENT_VERSION['laboratory_A_and_L']
+            model_version = _version['potato']['version']
+            model_name = _version['potato']['name']
             try:
                 for element in self.elements.keys():
                     quantity = self.elements[element]
@@ -57,14 +57,18 @@ class InFieldDecisionTree:
                     crop_name_encoded = crop_le.transform([crop_name])
                     predict = clf.predict([[element_encoded[0], crop_name_encoded[0], quantity]])
 
-                    values = predict[0].split('#')
+                    values = predict[0].split(';')
+                    level = values.pop(0)
+                    comments = values.pop(0)
 
-                    level = values[0]
-                    #comments = values[1]
-                    product = values[2]
-                    product_liters_per_hectare = values[3]
-                    water_liters_per_hectare = values[4]
-                    suggestion = values[5]
+                    products = []
+                    for _ in values:
+                        products.append({
+                            "product": values.pop(0),
+                            "product_liters_per_hectare": values.pop(0),
+                            "water_liters_per_hectare": values.pop(0),
+                            "suggestion": values.pop(0)
+                        })
 
                     recommendations.append(
                         {
@@ -72,10 +76,7 @@ class InFieldDecisionTree:
                             "level": level,
                             "quantity": self.elements[element],
                             "unit_measure": domain_element.unit_measure(element),
-                            "product": product,
-                            "product_liters_per_hectare": product_liters_per_hectare,
-                            "water_liters_per_hectare": water_liters_per_hectare,
-                            "suggestion": suggestion
+                            "product": products
                         }
                     )
                 self.repository.save_recommendation(id_field_analyses, recommendations, model_version, model_name)
--- machine_learning/laboratory_A_and_L/PotatoDecisionTree.py
+++ machine_learning/laboratory_A_and_L/PotatoDecisionTree.py
@@ -13,24 +13,24 @@ from sklearn.externals import joblib
 class PotatoDecisionTree:
 
     interactions = 27
-    file_name = 'machine_learning/laboratory_A_and_L/data/PotatoDecisionTree_Complete.csv'
+    file_name = 'machine_learning/laboratory_A_and_L/data/PotatoDecisionTree_Complete.xlsx'
 
     column_names = ['ELEMENT_ENCODED', 'CROP_NAME_ENCODED', 'QUANTITY']
 
-    QUANTITIES = [('VERY_LOW', 'VERY_LOW_COMMENTS', 'VL'),
-                  ('LOW', 'LOW_COMMENTS', 'L'),
-                  ('SLIGHTLY_LOW', 'SLIGHTLY_LOW_COMMENTS', 'SL'),
-                  ('NORMAL', 'NORMAL_COMMENTS', 'N'),
-                  ('HIGH', 'HIGH_COMMENTS', 'H'),
-                  ('VERY_HIGH', 'VERY_HIGH_COMMENTS', 'VH')]
+    QUANTITIES = [
+        ('VERY_LOW', 'VERY_LOW_COMMENTS', 'VL'),
+        ('LOW', 'LOW_COMMENTS', 'L'),
+        ('SLIGHTLY_LOW', 'SLIGHTLY_LOW_COMMENTS', 'SL'),
+        ('NORMAL', 'NORMAL_COMMENTS', 'N'),
+        ('HIGH', 'HIGH_COMMENTS', 'H')
+    ]
 
     NEXT_QUANTITIES = {
         'VERY_LOW': 'LOW',
         'LOW': 'SLIGHTLY_LOW',
         'SLIGHTLY_LOW': 'NORMAL',
         'NORMAL': 'HIGH',
-        'HIGH': 'VERY_HIGH',
-        'VERY_HIGH': ''
+        'HIGH': ''
     }
 
     def __init__(self):
@@ -41,38 +41,38 @@ class PotatoDecisionTree:
         self.crop_le = preprocessing.LabelEncoder()
 
     def pre_process(self):
-        self.df_train['ELEMENT'] = self.df_train['ELEMENT'].apply(str.strip)
-        self.df_train['CROP_NAME'] = self.df_train['CROP_NAME'].apply(str.strip)
-
         target = []
         features = []
 
         for index, row in self.df_train.iterrows():
             for quantity in self.QUANTITIES:
-                value = row[quantity[0]]
-                value = float(value.replace(',', '.'))
+                try:
+                    value = row[quantity[0]]
+                    if np.isnan(value):
+                        continue
 
-                if quantity[0] == 'VERY_HIGH':
-                    max_value = value + 999
-                else:
-                    max_value = row[self.NEXT_QUANTITIES[quantity[0]]]
-                    max_value = float(max_value.replace(',', '.'))
+                    if quantity[0] == 'HIGH':
+                        max_value = value + 10
+                    else:
+                        max_value = row[self.NEXT_QUANTITIES[quantity[0]]]
 
-                if np.isnan(value):
-                    continue
+                    for _ in range(0, self.interactions):
+                        copy_row = pd.Series(row).copy();
+                        copy_row['QUANTITY'] = value
+                        copy_row['ORIGINAL_QUANTITY'] = copy_row[quantity[0]]
+                        copy_row['QUANTITY_TYPE'] = quantity[0]
 
-                for _ in range(0, self.interactions):
-                    copy_row = pd.Series(row).copy();
-                    copy_row['QUANTITY'] = value
-                    copy_row['ORIGINAL_QUANTITY'] = copy_row[quantity[0]]
-                    copy_row['QUANTITY_TYPE'] = quantity[0]
+                        target_value = quantity[2] + ';' + copy_row[quantity[1]]
 
-                    features.append(copy_row)
-                    target.append(quantity[2] + '#' + copy_row[quantity[1]])
+                        target.append(target_value)
+                        features.append(copy_row)
 
-                    value = float(value + 0.005)
-                    if value >= max_value:
-                        break
+                        value = float(value + 0.005)
+                        if value >= max_value:
+                            break
+                except Exception as e:
+                    print(row)
+                    print(e)
 
         self.X = pd.DataFrame(data=features)
         self.y = pd.DataFrame(data=target, columns=['COMMENTS'])
@@ -84,9 +84,14 @@ class PotatoDecisionTree:
         self.crop_le.fit(self.df_train['CROP_NAME'])
         self.X['CROP_NAME_ENCODED'] = self.crop_le.transform(self.X['CROP_NAME'])
 
+        writer = pd.ExcelWriter('aqui.xlsx', engine='xlsxwriter')
+        self.X.to_excel(writer, sheet_name='Sheet1')
+        writer.save()
+
+
     def build_model(self):
         print('start building model')
-        self.df_train = pd.read_csv(self.file_name, sep=';')
+        self.df_train = pd.read_excel(self.file_name)
 
         self.pre_process()
         self.train_encode_labels()
@@ -108,6 +113,7 @@ class PotatoDecisionTree:
             print(row[['ELEMENT', 'QUANTITY_TYPE', 'QUANTITY']])
             print('\n')
         '''
+
         score = clf.score(X_train[self.column_names], y_train)
         print('SEU SCORE PARA BASE DE TREINO FOI DE {0:.4f}'.format(float(score)))
 
@@ -115,7 +121,7 @@ class PotatoDecisionTree:
         print('SEU SCORE PARA BASE DE TESTE FOI DE {0:.4f}'.format(float(score)))
 
         print('PERSISTINDO MODELO TREINADO, PARA FUTURO REUSO')
-        joblib.dump(clf, 'machine_learning/laboratory_A_and_L/PotatoDecisionTree.pkl')
+        joblib.dump(clf, 'machine_learning/laboratory_A_and_L/models/PotatoDecisionTree.pkl')
         joblib.dump(self.element_le, 'machine_learning/laboratory_A_and_L/models/PotatoDecisionTree_ElementLabelEncode.pkl')
         joblib.dump(self.crop_le, 'machine_learning/laboratory_A_and_L/models/PotatoDecisionTree_CropNameLabelEncode.pkl')
         print('model built')
